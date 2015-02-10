47eb4246a8b3d84ef83f66d8d5610dc1d82ec61d
antoineodier <odier@hab.de>
2015-02-10 16:36:42 +0100

    exercice_2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+
+article = Article.new(
+    title: Faker::Commerce.product_name,
+    content: Faker::Lorem.sentences(20)
+    )
+  article.save
+
+end
